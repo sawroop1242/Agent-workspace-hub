d24827574781d4da1625c7c948b97638c2b0051a
sawroop1242 <swroopss487@gmail.com>
2026-08-27 07:39:51 +0530

    cleanup(core): remove unused re-exports

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -4,10 +4,3 @@ pub mod memory;
 pub mod project;
 pub mod tasks;
 pub mod workspace;
-
-pub use context::ContextStore;
-pub use files::FileStore;
-pub use memory::MemoryStore;
-pub use project::ProjectStore;
-pub use tasks::TaskStore;
-pub use workspace::Workspace;
